bdd5f7886c6cdb730870505f81af71b84fc4b616
laolin <linjp@linjp.com>
2026-05-12 23:51:48 +0800

    导出包

---
+++ src/jgai_utils/__init__.py
@@ -0,0 +1,7 @@
+# Copyright (c) 2026 laolin. See LICENSE for details.
+
+__version__ = "0.1.0"
+
+from .printWithTime import printWithTime
+from .CalTime import CalTime
+from .generate_unique_id import generate_unique_id, generate_machine_id
