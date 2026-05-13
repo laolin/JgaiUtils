c59db2ab11b83980f6bc1665fe7695cc8601be97
laolin <linjp@linjp.com>
2026-05-13 22:31:15 +0800

    增加rint_by_seeds函数

--- src/jgai_utils/__init__.py
+++ src/jgai_utils/__init__.py
@@ -5,3 +5,4 @@ __version__ = "0.1.0"
 from .printWithTime import printWithTime
 from .CalTime import CalTime
 from .generate_unique_id import generate_unique_id, generate_machine_id
+from .rint_by_seeds import rint_by_seeds
---
+++ src/jgai_utils/rint_by_seeds.py
@@ -0,0 +1,23 @@
+# Copyright (c) 2026 laolin. See LICENSE for details.
+
+import zlib
+
+def rint_by_seeds(valueMax:int, *seedList):
+    """
+    根据seedList生成可复现的[0, iMax)随机整数
+    """
+    # 将所有种子转为字符串并拼接
+    seed_str = "jgai"+"!".join(str(s) for s in seedList)        
+    # zlib.crc32 返回一个固定的 32 位无符号整数
+    hash_val = zlib.crc32(seed_str.encode('utf-8'))      
+    return hash_val % valueMax
+
+if __name__ == "__main__":
+    print(1, rint_by_seeds(100, 1,2,3))
+    print(2, rint_by_seeds(200, 4,5,6,7,8,))
+    print(3, rint_by_seeds(300))
+    print(4, rint_by_seeds(400, 1,'aabbcc',[1,2,3],print))  # 可用任意变量做种子
+
+    print(5, rint_by_seeds(100, 1,2,3))  # 可复现第1行的结果
+    print(6, rint_by_seeds(300))  # 可复现第3行的结果
+    print(7, rint_by_seeds(400, 1,'aabbcc',[1,2,3],print))  # 可复现第4行结果
