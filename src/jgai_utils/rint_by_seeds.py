7a4dbdeac663f1f514201e0c56b44e0e0b9fa58e
laolin <linjp@linjp.com>
2026-06-03 08:50:26 +0800

    rint_by_seeds当valueMax<=0时取2**32-1

--- src/jgai_utils/rint_by_seeds.py
+++ src/jgai_utils/rint_by_seeds.py
@@ -10,7 +10,7 @@ def rint_by_seeds(valueMax:int, *seedList):
     seed_str = "jgai"+"!".join(str(s) for s in seedList)        
     # zlib.crc32 返回一个固定的 32 位无符号整数
     hash_val = zlib.crc32(seed_str.encode('utf-8'))      
-    return hash_val % valueMax
+    return hash_val % (valueMax if valueMax>0 else (2**32-1))
 
 if __name__ == "__main__":
     print(1, rint_by_seeds(100, 1,2,3))
@@ -21,3 +21,5 @@ if __name__ == "__main__":
     print(5, rint_by_seeds(100, 1,2,3))  # 可复现第1行的结果
     print(6, rint_by_seeds(300))  # 可复现第3行的结果
     print(7, rint_by_seeds(400, 1,'aabbcc',[1,2,3],print))  # 可复现第4行结果
+
+    print(0, rint_by_seeds(0, 1,2,3))
